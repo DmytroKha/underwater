055b0815e648eb9199a424a8d5e75acecd22c62f
DmytroKha <dmytro_khlypun@ukr.net>
2023-10-18 22:33:56 +0300

    API Implementation

--- internal/infra/database/migrations/20231017000002_create_sensor_readings_table.up.sql
+++ internal/infra/database/migrations/20231017000002_create_sensor_readings_table.up.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS public.sensor_readings (
     sensor_id INT REFERENCES sensors(id),
     timestamp TIMESTAMP NOT NULL,
     temperature FLOAT,
-    transparency INT,
-    fish_species JSONB
+    transparency INT
 );
